b6b588d69d5a16cfa481aeccad961f06f1a44fb5
Olivia Kesler <oakesler@protonmail.ch>
2019-11-05 20:43:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Write your code here
 
-let var1
-let var2
+const var1 = 2
+const var2 = 31
 
-const multiply = var1 * var2; 
+let multiply = var1 * var2; 
 let random = Math.random(); 
 let mod  = 9 % 5;
 let max = Math.max(1, 2, 20);
